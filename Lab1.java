ecef551cdcaa44cf82f52cdae87c991936ee68f3
elinanourmand <54960818+elinanourmand@users.noreply.github.com>
2020-08-24 13:47:42 -0400

    Delete Lab1.java

--- Lab1.java
+++
@@ -1,9 +0,0 @@
-/* This program will output "Hello World"
- */
-
-public class Lab1 {
-	public static void main (String [] args) {
-		System.out.print("Hello World");
-
-	}// close main
-}// close class
